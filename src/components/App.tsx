f420bfe4141b21fd55f6c3ae9a3734f11f42cc61
Ian Christopher B. de Jesus <ian.dejesus.md@gmail.com>
2020-04-12 19:35:30 +0900

    Add basic theming
    Theme and createMuiTheme for easy access to mui styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import Header from "./ui/header";
+import { theme } from "./ui/theme";
+import { ThemeProvider } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header /> Hello!
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ui/theme.tsx
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#0B72B9"
+    },
+    secondary: {
+      main: "#FFBA60"
+    }
+  }
+});
